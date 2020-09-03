23a467d039ff1055f606d006b29305868521cc0f
rifnur <rifnur69v@mail.ru>
2020-09-03 07:42:49 +0300

    Уровень 2 Урок 4

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import java.awt.*;
+
+public class Main {
+    public static void main(String[] args) {
+        //Main.class.getClassLoader().getResource("images/logo.png");
+
+        new Sms();
+    }
+}
+
